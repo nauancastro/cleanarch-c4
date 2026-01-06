5f63d7f636f14ba90ffc65772f7d11d6e7147e75
Nauan Castro <nauancastro@alu.ufc.br>
2026-01-06 02:17:31 -0300

    Ajuste para vps

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
     // If you use Nginx to proxy /api to the backend, you can use relative path '/api'
     // Or the full URL: 'https://api.your-domain.com'
     // apiUrl: 'http://localhost:7777'
-    apiUrl: 'https://api.malibu.nauan.dev.com'
+    apiUrl: 'https://apimalibu.nauan.dev.com'
 };
